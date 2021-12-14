1c2db3c3cc9155ae87282d2b170188fe107c03b0
victormessiasaraujo <victor.messias.araujo@gmail.com>
2021-12-13 23:17:04 -0300

    #Sessao 5

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,9 +10,35 @@ export class EventosComponent implements OnInit {
 
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+  private _filtroLista: string = '';
+
+  exibirImagem: boolean = true;
   larguraImagem: number = 100;
   margemImagem: number = 2;
-  exibirImagem: boolean = true;
+
+  public get filtroLista():string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+
+    return this.eventos.filter((
+        evento: {
+          tema: string;
+          local : string
+        }
+      ) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+           evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -26,7 +52,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos() : void {
     this.http.get('https://localhost:5001/api/Eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
     );
   }
