9d4b5d857b63f5c77a2698e53e9d99eb6405dd60
victormessiasaraujo <victor.messias.araujo@gmail.com>
2022-02-02 13:20:10 -0300

    ###

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,11 +1,4 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService } from 'ngx-spinner';
-
-import { Evento } from '../../models/Evento';
-import { EventoService } from '../../services/evento.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
